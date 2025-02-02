4005ef4740daef6e70c00c62a517fcce7fba138d
Sahran <94926019+SahranREAL@users.noreply.github.com>
2025-02-02 13:19:58 +0100

    so much lol

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,66 +1,100 @@
-/*=============== FILTERS TABS ===============*/
-const tabs = document.querySelectorAll('[data-target]'),
-      tabContents = document.querySelectorAll('[data-content]')
+import { initScrollReveal } from "./scrollReveal.js";
+import { hoverChangeExperience } from "./hoverChangeExperience.js";
+import { typeWrite } from "./typeWrite.js";
+import { hoverChangeDescription } from "./hoverChangeDescription.js";
+import { menu } from "./menu.js";
 
-tabs.forEach(tab =>{
-    tab.addEventListener('click', () =>{
-        const target = document.querySelector(tab.dataset.target)
+menu();
+initScrollReveal();
+typeWrite(document.querySelector(".typewriter"));
 
-        tabContents.forEach(tc =>{
-            tc.classList.remove('filters__active')
-        })
-        target.classList.add('filters__active')
+hoverChangeExperience(
+  ".digitalhouse",
+  `Trabalhei como instrutor conteudista em frontend na
+  Digital House, o trabalho consistia em criar conteúdos para as
+  aulas de especialização frontend. Alguns temas dos conteúdos
+  realizados foram: React, Redux, TypeScript, Testes,
+  GraphQL, Next.js, MUI, React Hook Form e styled-components.`,
+  "Professor conteudista em Frontend",
+  "Digital House",
+  "Nov 2021 - Nov 2022 (1 ano)"
+);
 
-        tabs.forEach(t =>{
-            t.classList.remove('filter-tab-active')
-        })
-        tab.classList.add('filter-tab-active')
-    })
-})
+hoverChangeExperience(
+  ".zuplae",
+  `Trabalhei como Social Media e Designer na Zuplae que é um escola
+  de programação, onde teve como foco as criações de conteúdos sobre
+  programação para a comunidade dev através das redes sociais.`,
+  "Social Media e UI Designer",
+  "Zuplae",
+  "Jan 2022 - Abril 2022 (4 meses)"
+);
 
-/*=============== DARK LIGHT THEME ===============*/
-const themeButton = document.getElementById('theme-button')
-const darkTheme = 'dark-theme'
-const iconTheme = 'ri-sun-line'
+hoverChangeExperience(
+  ".codigofontetv",
+  `Trabalhei como Social Media e Designer no Código Fonte TV, 
+  onde teve como foco as criações de conteúdos sobre programação 
+  para a comunidade dev através das redes sociais.`,
+  "Social Media e Designer",
+  "Código Fonte TV",
+  "Jun 2021 - Jan 2022 (8 meses)"
+);
 
-// Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
+hoverChangeExperience(
+  ".contweb",
+  `Realizei o estágio na ContWeb, uma empresa de contabilidade. 
+  Onde pela primeira vez tive experiência com o React. Além do
+  desenvolvimento frontend criei os designs da nova plataforma da empresa.`,
+  "Developer frontend e UI Designer",
+  "ContWeb",
+  "Set 2021 - Nov 2021 (3 meses)"
+);
 
-// We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
-
-// We validate if the user previously chose a topic
-if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
-}
-
-// Activate / deactivate the theme manually with the button
-themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
-})
-
-/*=============== SCROLL REVEAL ANIMATION ===============*/
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '60px',
-    duration: 2500,
-    delay: 400,
-})
-
-sr.reveal(`.profile__border`)
-sr.reveal(`.profile__name`, {delay: 500})
-sr.reveal(`.profile__profession`, {delay: 600})
-sr.reveal(`.profile__social`, {delay: 700})
-sr.reveal(`.profile__info-group`, {interval: 100, delay: 700})
-sr.reveal(`.profile__buttons`, {delay: 800})
-sr.reveal(`.filters__content`, {delay: 900})
-sr.reveal(`.filters`, {delay: 1000})
+hoverChangeDescription(
+  ".html",
+  "HTML é uma linguagem de marcação, onde marcamos os elementos para definir quais informações a página vai exibir."
+);
+hoverChangeDescription(
+  ".css",
+  "CSS é uma linguagem de folha de estilo composta por “camadas”, criado com o propósito de estilizar as páginas."
+);
+hoverChangeDescription(
+  ".js",
+  "JavaScript é uma linguagem de programação que permite a você implementar elementos dinâmicos em páginas web."
+);
+hoverChangeDescription(
+  ".sass",
+  "Sass é um pré-processador CSS que adiciona alguns recursos que não estão disponíveis de forma nativa."
+);
+hoverChangeDescription(
+  ".react",
+  "React é uma biblioteca JavaScript com foco em criar interfaces de usuário de forma componetizada."
+);
+hoverChangeDescription(
+  ".next",
+  "Next.js é uma estrutura da web que permite funcionalidades como renderização do lado do servidor e geração de sites estáticos para web baseados em React."
+);
+hoverChangeDescription(
+  ".styled",
+  "styled-components é uma biblioteca que utiliza o conceito de CSS-in-JS, ou seja, que nos permite escrever códigos CSS dentro do Javascript."
+);
+hoverChangeDescription(
+  ".tailwind",
+  "Tailwind CSS é um framework CSS que nos fornece classes utilitárias com o propósito de estilizar as páginas."
+);
+hoverChangeDescription(
+  ".typescript",
+  "TypeScript é um superset do JavaScript que incluir recursos que não estão presentes na linguagem de forma nativa, além de torná-la estática."
+);
+hoverChangeDescription(
+  ".radix",
+  "Radix é uma biblioteca que fornece componentes acessíveis e sem estilos para a criação de aplicação em React."
+);
+hoverChangeDescription(
+  ".cypress",
+  "Cypress é um framework para automação de testes end-to-end, onde atualmente usa a linguagem JavaScript."
+);
+hoverChangeDescription(
+  ".storybook",
+  "Storybook é uma ferramenta que tem como objetivo documentar e testar os componentes da aplicação."
+);
